1b2275899273045a7b56ac0472d4cf98b5032743
SivakumarBalu <postboxno100@gmail.com>
2017-03-22 15:34:17 -0700

    phonelist component created along with its spec

--- app/app.js
+++ app/app.js
@@ -1,20 +1,2 @@
 //define the phonecatApp module
 var phonecatApp = angular.module('phonecatApp',[]);
-
-//define the PhoneListController in phonecatApp module
-phonecatApp.controller('PhoneListController', function($scope){
-    $scope.phones = [
-        {
-            name: "Nexus S",
-            snippet: "Fast just got faster with Nexus S."
-        },
-        {
-            name: "Motorloa XOOM with wifi",
-            snippet: "The Next, Next Generation tablet."
-        },
-        {
-            name: "Motorloa XOOM",
-            snippet: "The Next, Next Generation tablet."
-        }
-    ];
-});
